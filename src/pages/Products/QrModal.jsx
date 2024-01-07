d5f706e885b0f580f50ba8ede049321c94d7fd08
Suyel Haque <slsuyel@gmail.com>
2024-01-08 01:57:06 +0600

    Download Qr code image

--- src/pages/Products/QrModal.jsx
+++ src/pages/Products/QrModal.jsx
@@ -1,16 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import qr from '../../assets/icons/png/QR-icon.png';
 
 const QrModal = ({ showModal, handleShow, handleClose, product }) => {
-    const qrCodeRef = useRef();
+    const [qrCodeUrl, setQrCodeUrl] = useState('');
 
     useEffect(() => {
         if (showModal && product) {
-            const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(JSON.stringify(product))}&size=300x300`;
-            qrCodeRef.current.src = qrCodeUrl;
+            const newQrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(JSON.stringify(product))}&size=300x300`;
+            setQrCodeUrl(newQrCodeUrl);
         }
     }, [showModal, product]);
+
+    const downloadQrCodeImage = () => {
+        fetch(qrCodeUrl)
+            .then(response => response.blob())
+            .then(blob => {
+                const a = document.createElement('a');
+                a.href = window.URL.createObjectURL(blob);
+                a.download = 'qr_code.png';
+                a.style.display = 'none';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            })
+            .catch(error => console.error('Error downloading QR code image:', error));
+    };
+
+
 
     return (
         <>
@@ -20,14 +37,14 @@ const QrModal = ({ showModal, handleShow, handleClose, product }) => {
                 </Modal.Header>
                 <Modal.Body className='mx-auto'>
                     {product && (
-                        <img ref={qrCodeRef} src={qr} className='img-fluid' alt="QR Code" />
+                        <img src={qrCodeUrl || qr} className='img-fluid' alt="QR Code" />
                     )}
                 </Modal.Body>
                 <Modal.Footer className='justify-content-around'>
-                    <Button className='fw-bold text-white' variant="warning" >
+                    <Button className='fw-bold text-white' variant="warning" onClick={downloadQrCodeImage}>
                         Download
                     </Button>
-                    <Button className='fw-bold text-white ' style={{ background: '#22a6bb' }} >
+                    <Button className='fw-bold text-white' style={{ background: '#22a6bb' }}>
                         Flipping Book preview
                     </Button>
                 </Modal.Footer>
